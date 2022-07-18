a5bc6277d686c684d94f77c154a6a7cbab156d9a
Luke G <lukejgranered@gmail.com>
2022-07-18 11:01:58 -0500

    doodler jumping and landing

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const doodler = document.createElement('div')
     let doodlerLeftSpace = 50
-    let doodlerBottomSpace = 150
+    let doodlerBottomSpace = 250
     let isGameOver = false
     let platformCount = 5
     let platforms = []
+    let upTimerId
+    let downTimerId
+    let isJumping = true
 
     function createDoodler() {
         grid.appendChild(doodler)
         doodler.classList.add('doodler')
+        doodlerLeftSpace = platforms[0].left
         doodler.style.left = doodlerLeftSpace + 'px'
         doodler.style.bottom = doodlerBottomSpace + 'px'
     }
@@ -34,14 +38,79 @@ document.addEventListener('DOMContentLoaded', () => {
             let newPlatBottom = 100 + i * platformGap
             let newPlatform = new Platform(newPlatBottom)
             platforms.push(newPlatform)
+            
         }
     }
 
+    function movePlatforms() {
+        if (doodlerBottomSpace > 200) {
+            platforms.forEach(platform => {
+                platform.bottom -= 4
+                let visual = platform.visual
+                visual.style.bottom = platform.bottom + 'px'
+            })
+        }
+    }
+
+    function jump() {
+        clearInterval(downTimerId)
+        isJumping = true
+        upTimerId = setInterval(function () {
+            doodlerBottomSpace += 20
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+            if (doodlerBottomSpace > 350) {
+                fall()
+            }
+        }, 30)
+    }
+
+    function fall() {
+        clearInterval(upTimerId)
+        isJumping = false
+        downTimerId = setInterval(function () {
+            doodlerBottomSpace -= 5
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+            if (doodlerBottomSpace <= 0) {
+                gameOver()
+            }
+            platforms.forEach(platform => {
+                if (
+                        (doodlerBottomSpace >= platform.bottom) &&
+                        (doodlerBottomSpace <= platform.bottom + 15) &&
+                        ((doodlerLeftSpace + 60) >= platform.left) &&
+                        (doodlerLeftSpace <= (platform.left + 85)) &&
+                        !isJumping
+                    ) {
+                        console.log('landed')
+                        jump()
+                    }
+            })
+        }, 30)
+    }
+
+    function gameOver() {
+        console.log('game over')
+        isGameOver = true
+        clearInterval(upTimerId)
+        clearInterval(downTimerId)
+    }
+
+    function control(e) {
+        if (e.key === 'ArrowLeft') {
+            //move left
+        } else if (e.key === 'ArrowRight') {
+            //move right
+        } else if (e.key === 'ArrowUp') {
+            //move straight
+        }
+    }
 
     function start() {
         if (isGameOver == false) {
-            createDoodler()
             createPlatforms()
+            createDoodler()
+            setInterval(movePlatforms, 30)
+            jump()
         } 
     }
     // attach to a button
